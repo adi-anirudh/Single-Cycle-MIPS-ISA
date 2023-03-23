c7b36b93fc2ee435aee9c9b1036e41b6d16e37cd
adi-anirudh <74549709+adi-anirudh@users.noreply.github.com>
2023-03-23 15:20:47 +0530

    Update ALUControl.v

--- ALUControl.v
+++ ALUControl.v
@@ -1,23 +1,23 @@
 module ALUControl(ALU_Control, ALUOp, Function);
-output reg[2:0] ALU_Control;
+    output reg[3:0] ALU_Control;
 input [1:0] ALUOp;
-input [3:0] Function;
+input [5:0] Function;
 wire [5:0] ALUControlIn;
 assign ALUCOntrolIn = {ALUOp, Function};
 always@(ALUControlIn)
 casex(ALUControlIn)
-    6'b11xxxx: ALU_Control=3'b000;
-    6'b10xxxx: ALU_Control=3'b100;
-    6'b01xxxx: ALU_Control=3'b001;
-    6'b000000: ALU_Control=3'b000;
-    6'b000001: ALU_Control=3'b001;
-    6'b000010: ALU_Control=3'b010;
-    6'b000011: ALU_Control=3'b011;
-    6'b000100: ALU_Control=3'b100;
+    8'b00xxxxxx: ALU_Control=4'b0010;// add for LW and SW
+    8'b01xxxxxx: ALU_Control=4'b0110;// sub for branch equal
+    8'b10100000: ALU_Control=4'b0010;// R type add
+    8'b10100010: ALU_Control=4'b0110;// R type sub
+    8'b10100100: ALU_Control=3'b0000;// R type and
+    8'b10100101: ALU_Control=3'b0001;// R type or
+    8'b10101010: ALU_Control=3'b0111;// R type slti
+    
     default: ALU_Control=3'b000;
 endcase
 endmodule
 
-module JR_Control(input [1:0] alu_op, input[3:0] funct, output JRControl);
-assign JR_Control=({alu_op,funct}==6b'001000) ? 1'b1 : 1'b0;
+module JR_Control(input [1:0] alu_op, input[5:0] funct, output JRControl);
+    assign JR_Control=({alu_op,funct}==8b'11100000) ? 1'b1 : 1'b0;
 endmodule
